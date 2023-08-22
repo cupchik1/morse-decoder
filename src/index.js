f66a51584ec6eae962d7df72de0f47fb8cf551f8
Alina Bezuhla <alinabezuglaya123@gmail.com>
2023-08-23 01:00:35 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    let i = 0;
+    let j = 2;
+    while (expr) {
+    if (expr.slice(i, j)) {
+        if (+expr.slice(i, j)) {
+            array.push(String(Number(expr.slice(i, j))));
+        }
+        // else if (expr.slice(i, j) == "**" && expr.slice(i-2, j-2) != "**"){
+        //     array.push(" ");
+        // }
+        else {
+            array.push(" ");
+        }
+        i = j;
+        j = j + 2;
+    }
+    else break;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "10") {
+            array[i] = ".";
+        }
+        else if (array[i] === "11") {
+            array[i] = "-";
+        }
+    }
+    array = array.join('');
+    let morseArray = [];
+    for (let i = 0; i < array.length; i = i + 5) {
+        morseArray.push(array.slice(i, i + 5).trimStart());
+    }
+    for (let i = 0; i < morseArray.length; i++) {
+        for (let key in MORSE_TABLE) {
+            if (morseArray[i] === key) {
+                morseArray[i] = MORSE_TABLE[key];
+            }
+        }
+    }
+    for (let i = 0; i < morseArray.length; i++) {
+        if (morseArray[i] === '')
+            morseArray[i] = " ";
+    }
+    morseArray = morseArray.join('');
+    return morseArray;
 }
 
 module.exports = {
